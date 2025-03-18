cc034484292ad0026bb2842c2ecafdcf40a0266e
jurraca <jurraca@users.noreply.github.com>
2025-03-18 14:34:01 +0000

    add flake for dev environment

--- flake.nix
+++ flake.nix
@@ -1,10 +1,22 @@
 {
   description = "Kartograf workshop";
-  inputs = "";
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    kartograf.url = "github:asmap/kartograf";
+    rpki-client.url = "github:asmap/rpki-client-nix";
+    rpki-client.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = {
-
-    
-  }
-  
+  outputs = { self, nixpkgs, kartograf, rpki-client }: let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    kartograf-cli = kartograf.packages.${system}.default;
+    rpki-cli = rpki-client.defaultPackage.${system};
+    in {
+    devShells = {
+      ${system}.default = pkgs.mkShell {
+        packages = [ kartograf-cli rpki-cli ];
+      };
+    };
+  };
 }
