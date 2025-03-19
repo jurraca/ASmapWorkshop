ef28de6b7bf9550b055c31a5476f31ee407f5c74
jurraca <jurraca@users.noreply.github.com>
2025-03-19 11:35:04 +0000

    flake: follow local nixpkgs, simplify, format

--- flake.nix
+++ flake.nix
@@ -3,20 +3,25 @@
   inputs = {
     nixpkgs.url = "nixpkgs";
     kartograf.url = "github:asmap/kartograf";
+    kartograf.inputs.nixpkgs.follows = "nixpkgs";
     rpki-client.url = "github:asmap/rpki-client-nix";
     rpki-client.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, kartograf, rpki-client }: let 
+  outputs = {
+    self,
+    nixpkgs,
+    kartograf,
+    rpki-client,
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    kartograf-cli = kartograf.packages.${system}.default;
-    rpki-cli = rpki-client.defaultPackage.${system};
-    in {
-    devShells = {
-      ${system}.default = pkgs.mkShell {
-        packages = [ kartograf-cli rpki-cli ];
-      };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          kartograf.packages.${system}.default
+          rpki-client.defaultPackage.${system}
+        ];
     };
   };
 }
